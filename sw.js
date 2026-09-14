afd14541273ca2f2011048c7541177e513080d5b
silka47 <kjy0325kk@gmail.com>
2026-09-15 00:11:54 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'ssin-diary-v2';
+const CACHE_NAME = 'ssin-diary-v3';
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
@@ -7,7 +7,6 @@ const ASSETS_TO_CACHE = [
   './manifest.json'
 ];
 
-// 설치 시 캐시 저장 및 즉시 활성화
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS_TO_CACHE))
@@ -15,7 +14,6 @@ self.addEventListener('install', (event) => {
   self.skipWaiting();
 });
 
-// 활성화 시 이전 구버전(v1 등) 캐시 강제 삭제
 self.addEventListener('activate', (event) => {
   event.waitUntil(
     caches.keys().then((keys) => {
@@ -27,7 +25,6 @@ self.addEventListener('activate', (event) => {
   self.clients.claim();
 });
 
-// 네트워크 우선(Network-First) 전략: 인터넷 연결 시 항상 최신 코드를 가져옴
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     fetch(event.request)
